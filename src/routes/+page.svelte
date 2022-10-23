cd1afb74fa59d8ea5533f47c203092ad5fcdad6d
jamespurnama1 <james.henry.purnama0@gmail.com>
2022-10-24 00:50:33 +0700

    Landing page WIP
    TODO:
    -change colors
    -animate BG
    -fix cursor scroll
    -responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { gsap } from "gsap";
+	// import { ScrollTrigger } from "gsap/ScrollTrigger";
+	import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
+
+// import { gsap, ScrollTrigger } from "../scripts/gsap";
+
+// Data
+let portfolios = [
+	'Revolver',
+	'Wizly',
+	'Lovespun',
+	'Gabongo',
+	'Hamamoto',
+];
+
+function handleHover(index: number) {
+	switch(index) {
+		case 1:
+			//change BG
+			break;
+	}
+}
+
+//scrolltrigger
+onMount(() => {
+	gsap.registerPlugin(ScrollTrigger);
+	gsap.to(".bg", {
+		scrollTrigger: '.portfoliosList',
+		backgroundImage: '',
+	})
+})
+</script>
+
+<style>
+	.bg {
+		background-image: url('/assets/220212_DEB_01_Logo-1536x988.jpg');
+	}
+</style>
+
+<main class="min-h-screen">
+	<ul class="portfoliosList m-auto text-center group">
+		{#each portfolios as portfolio, i}
+			<li on:mouseover={() => handleHover(i)} on:focus={() => handleHover(i)} class="my-12">
+				<a href="/portfolio/{portfolio}" class="transition-all group-hover:opacity-50 hover:!opacity-100">
+					<h1 class="listChildren text-5xl font-semibold text-white">{portfolio}</h1>
+				</a>
+			</li>
+		{/each}
+	</ul>
+</main>
+<div class="bg min-h-screen fixed top-0 left-0 w-screen -z-10" />
