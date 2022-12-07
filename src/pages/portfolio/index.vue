e994870c5978b8714e23473eb00e422621511748
jamespurnama1 <james.henry.purnama0@gmail.com>
2022-12-07 13:25:03 +0700

    fix duplicate portfolio list

--- src/pages/portfolio/index.vue
+++ src/pages/portfolio/index.vue
@@ -16,7 +16,8 @@
   async function load() {
      axios.get('https://api.cosmicjs.com/v2/buckets/comomaya-production/objects?query=%7B%22type%22%3A%22portfolio-list%22%2C%22slug%22%3A%22list%22%7D&pretty=true&read_key=a59I38Pp6PQ3OIRd6QnAQNvatVHRuIAfN3dzAnv8bFMD7p0qAF&props=metadata')
       .then((res: AxiosResponse<List>) => {
-        response.list.push(...res.data.objects[0].metadata.list)
+        response.list = res.data.objects[0].metadata.list
+        console.log(response.list.length)
       }).catch((err) => {
         console.error(err)
       })
