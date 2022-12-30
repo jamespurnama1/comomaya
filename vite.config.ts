0a9e92c6cb50588e0a2187a8e0d3793a49b9de9f
jamespurnama1 <james.henry.purnama0@gmail.com>
2022-12-31 01:02:10 +0700

    plasysinline updated npm ssl localhost fix cursor safari

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages()],
+  plugins: [vue(), Pages(), basicSsl()],
   ssgOptions: {
     crittersOptions: {
       // E.g., change the preload strategy
