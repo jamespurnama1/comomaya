21b05f7968f1ef082f7c5863c9bdf96a17746a78
jamespurnama1 <james.henry.purnama0@gmail.com>
2023-01-17 15:35:40 +0700

    404 page

---
+++ src/pages/[notFound].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+router.push('/404')
+</script>
+
+<template>
+  <main class="relative min-h-screen flex flex-col items-center justify-center py-20">
+    <h1 class="text-3xl md:text-5xl">Page not found</h1>
+    <a aria-label="Back to landing page" class="underline hover:text-active" href="/">go back to home page</a>
+  </main>
+</template>
