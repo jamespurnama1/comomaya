ce734b094068aa4d2541d5c68a2430b26f4cba23
jamespurnama1 <james.henry.purnama0@gmail.com>
2023-06-07 15:31:20 +0700

    Update Cosmic API

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -28,9 +28,9 @@ export const useStore = defineStore('store', {
     async load() {
       if (this.featured.length) return
       axios
-      .get('https://api.cosmicjs.com/v2/buckets/comomaya-production/objects?query=%7B%22type%22%3A%22landing%22%2C%22slug%22%3A%22home%22%7D&pretty=true&read_key=a59I38Pp6PQ3OIRd6QnAQNvatVHRuIAfN3dzAnv8bFMD7p0qAF&props=metadata')
+      .get('https://api.cosmicjs.com/v3/buckets/comomayacom-production/objects/64803dec2fb5fafdbb9670bb?read_key=Yz8ifYSRHxv4SzRygKNMbdGZnUaTUAUZBbseBGOILB3eWpiwh1&depth=1&props=metadata')
       .then((res) => {
-        this.featured = res.data.objects[0].metadata.featured
+        this.featured = res.data.object.metadata.featured
       }).catch((err) => {
         this.featured = []
         console.error(err)
