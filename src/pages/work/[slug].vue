ca0346d8c407f18729459bcfbd98b703869392c4
jamespurnama1 <james.henry.purnama0@gmail.com>
2023-06-07 15:31:20 +0700

    Update Cosmic API

--- src/pages/work/[slug].vue
+++ src/pages/work/[slug].vue
@@ -44,8 +44,8 @@
   store.load()
 
  	async function load() {
-      axios.get('https://api.cosmicjs.com/v2/buckets/comomaya-production/objects?pretty=true&query=%7B%22type%22%3A%22portfolios%22%7D&read_key=a59I38Pp6PQ3OIRd6QnAQNvatVHRuIAfN3dzAnv8bFMD7p0qAF&limit=20&props=slug,title,content,metadata', { withCredentials: false })
-      .then((res: AxiosResponse<{ objects: Content[] }>) => {
+      axios.get('https://api.cosmicjs.com/v3/buckets/comomayacom-production/objects?pretty=true&query=%7B%22type%22:%22portfolios%22%7D&limit=10&read_key=Yz8ifYSRHxv4SzRygKNMbdGZnUaTUAUZBbseBGOILB3eWpiwh1&depth=1&props=slug,title,metadata,content', { withCredentials: false })
+      .then((res: AxiosResponse<{ object: Content[] }>) => {
         contentID = res.data.objects.map(x => x.slug).indexOf(route.params.slug as string);
         (thisPage as { content: Content }).content = res.data.objects[contentID];
       }).catch((err) => {
