d082e32ed8db28cdfcf0360aefd1d36e8b91b59d
jamespurnama1 <james.henry.purnama0@gmail.com>
2024-03-29 11:53:17 +0700

    sizing on 720p and smaller devices fix scrolling to grants & services fix home screen layout fix widow fix TM added green dots

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -38,20 +38,20 @@ onUnmounted(() => {
 
 <template>
   <footer
-    class="flex flex-col items-start justify-start py-12 md:py-24 px-5 md:px-36 lg:mx-auto w-screen gap-8 md:gap-24"
+    class="flex flex-col items-start justify-start py-12 md:py-24 px-9 lg:px-20 2xl:px-36 lg:mx-auto w-screen gap-12 md:gap-24"
     :class="[$route.name === 'work-slug' ? 'bg-stone-300 text-active' : 'bg-beige-lighter text-black']">
     <!-- <video class="recolor max-h-10 md:max-h-24 md:-ml-3" playsinline loop autoplay="true" muted>
       <source src="/assets/aboutus.mp4" type="video/mp4" />
     </video> -->
     <!-- <h3 class="text-blue text-base md:text-xl font-semibold tracking-widest text-center my-16">FOLLOW US ON
         INSTAGRAM&nbsp;&amp;&nbsp;LINKEDIN</h3> -->
-    <ul class="flex gap-24 md:flex-row flex-col">
+    <ul class="flex gap-8 md:gap-24 md:flex-row flex-col">
       <!-- <li class="text-beige-lighter hover:text-active text-lg"><router-link to="/">Home</router-link></li> -->
       <li class="group"><router-link to="/work">
           <div>
             <p class="text-lg text-stone-500 font-semibold tracking-widest">
               WORK</p>
-            <p class="font-extrabold text-3xl md:text-5xl tracking-tight"
+            <p class="font-extrabold text-3xl 2xl:text-5xl tracking-tight"
               :class="[$route.name === 'work-slug' ? 'group-hover:text-blue' : 'group-hover:text-active']">
               See it here.</p>
           </div>
@@ -60,7 +60,7 @@ onUnmounted(() => {
           <div>
             <p class="text-lg text-stone-500 font-semibold tracking-widest">
               ABOUT</p>
-            <p class="font-extrabold text-3xl md:text-5xl tracking-tight -ml-1"
+            <p class="font-extrabold text-3xl 2xl:text-5xl tracking-tight -ml-1"
               :class="[$route.name === 'work-slug' ? 'group-hover:text-blue' : 'group-hover:text-active']">
               Our services.</p>
           </div>
@@ -71,34 +71,34 @@ onUnmounted(() => {
           <div>
             <p class="text-lg text-stone-500 font-semibold tracking-widest">
               CONTACT</p>
-            <p class="font-extrabold text-3xl md:text-5xl tracking-tight"
+            <p class="font-extrabold text-3xl 2xl:text-5xl tracking-tight"
               :class="[$route.name === 'work-slug' ? 'group-hover:text-blue' : 'group-hover:text-active']">
               Let's chat.</p>
           </div>
         </router-link></li>
     </ul>
-    <ul class="flex gap-3 md:gap-10 font-extrabold tracking-tight text-2xl md:text-3xl">
+    <ul class="flex gap-3 md:gap-10 font-extrabold tracking-tight text-2xl 2xl:text-3xl">
       <li>Singapore.</li>
       <li>India.</li>
     </ul>
     <div class="flex justify-between w-full md:flex-row flex-col-reverse gap-5">
-      <p class="text-stone-500 md:text-base text-sm">
+      <p class="text-stone-500 2xl:text-base text-sm">
         © 2024 COMOMAYA. All rights reserved.
       </p>
       <div class="flex gap-1 md:gap-5 md:text-base text-sm">
-        <a class="z-10 relative" key="ig" href="https://instagram.com/comomaya"
+        <a class="z-10 relative text-stone-500 hover:text-active" key="ig" href="https://instagram.com/comomaya"
           aria-label="Open Comomaya's Instagram Page" target="_blank" rel="noopener noreferrer">
           <font-awesome-icon :icon="['fab', 'square-instagram']" size="xl"
-            class="mx-2 my-3 text-stone-500 hover:text-active transition-all duration-200" />
+            class="mx-2 my-3 transition-all duration-200" />
           <!-- <p class="absolute group-hover:opacity-100 opacity-0 text-active font-bold">(INSTAGRAM)</p>
           <p class="text-beige-lighter group-hover:opacity-0">(INSTAGRAM)</p> -->
         </a>
-        <a class="z-10 relative" key="linkedin" href="https://www.linkedin.com/company/comomaya"
-          aria-label="Open Comomaya's Linkedin Page" target="_blank" rel="noopener noreferrer">
+        <a class="z-10 relative text-stone-500 hover:text-active" key="linkedin"
+          href="https://www.linkedin.com/company/comomaya" aria-label="Open Comomaya's Linkedin Page" target="_blank"
+          rel="noopener noreferrer">
           <!-- <p class="absolute group-hover:opacity-100 opacity-0 text-active font-bold">(LINKEDIN)</p>
           <p class="text-beige-lighter group-hover:opacity-0">(LINKEDIN)</p> -->
-          <font-awesome-icon :icon="['fab', 'linkedin']" size="xl"
-            class="mx-2 my-3 text-stone-500 hover:text-active transition-all duration-200" />
+          <font-awesome-icon :icon="['fab', 'linkedin']" size="xl" class="mx-2 my-3 transition-all duration-200" />
         </a>
       </div>
     </div>
