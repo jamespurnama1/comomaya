0cbffd525a1a24a934cf389869c1a609fa276896
jamespurnama1 <james.henry.purnama0@gmail.com>
2024-08-11 17:50:45 +0700

    Form serverless function with error UI
    TODO:
    Thank you notification
    Pop up contact

--- api/contact/index.ts
+++ api/contact/index.ts
@@ -1,75 +1,66 @@
+import * as nodemailer from 'nodemailer';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(
+export default async function handler(
   request: VercelRequest,
   response: VercelResponse,
 ) {
+
+
+const transporter = nodemailer.createTransport({
+      port: 465,
+      host: "smtp.gmail.com",
+      auth: {
+          user: import.meta.env.GMAIL_SEND,
+          pass: import.meta.env.GMAIL_PASSWORD,
+      },
+      secure: true,
+  });
+
+    // const body = await readBody(event)
+    const { name, email, message } = request.body;
+
+  await new Promise((resolve, reject) => {
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+          if (error) {
+              console.log(error);
+              reject(error);
+          } else {
+              resolve(success);
+          }
+      });
+  });
+
+  const mailData = {
+      from: {
+        name,
+        address: import.meta.env.GMAIL_SEND,
+      },
+      replyTo: email,
+      to: import.meta.env.GMAIL_RECEIVE,
+      subject: `New Message on COMOMAYA.com - ${name}`,
+      text: message,
+      html: message,
+  };
+
+  await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailData, (err, info) => {
+          if (err) {
+              console.error(err);
+              reject(err);
+          } else {
+              console.log(info);
+              resolve(info);
+          }
+      });
+  });
+
   response.status(200).json({
     body: request.body,
     query: request.query,
     cookies: request.cookies,
   });
+
 }
-
-// const transporter = nodemailer.createTransport({
-//       port: 465,
-//       host: "smtp.gmail.com",
-//       auth: {
-//           user: config.GMAIL_SEND,
-//           pass: config.GMAIL_PASSWORD,
-//       },
-//       secure: true,
-//   });
-
-// export default function handler(
-//   request: VercelRequest,
-//   response: VercelResponse,
-// ) {
-//   response.status(200).json({
-//     body: request.body,
-//     query: request.query,
-//     cookies: request.cookies,
-//   });
-// }
-
-//     const body = await readBody(event)
-//     const { name, email, message } = body;
-
-//   await new Promise((resolve, reject) => {
-//       // verify connection configuration
-//       transporter.verify(function (error, success) {
-//           if (error) {
-//               console.log(error);
-//               reject(error);
-//           } else {
-//               resolve(success);
-//           }
-//       });
-//   });
-
-//   const mailData = {
-//       from: {
-//         name,
-//         address: config.GMAIL_SEND,
-//       },
-//       replyTo: email,
-//       to: config.GMAIL_RECEIVE,
-//       subject: `New Message on COMOMAYA.com - ${name}`,
-//       text: message,
-//       html: message,
-//   };
-
-//   await new Promise((resolve, reject) => {
-//       // send mail
-//       transporter.sendMail(mailData, (err, info) => {
-//           if (err) {
-//               console.error(err);
-//               reject(err);
-//           } else {
-//               console.log(info);
-//               resolve(info);
-//           }
-//       });
-//   });
-
-// //   res.status(200).json({ status: "OK" });
