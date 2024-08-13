c878ab2fe00ddb9c746ba6bcdc61aab7c668bd5d
jamespurnama1 <james.henry.purnama0@gmail.com>
2024-08-14 00:47:09 +0700

    closeModal after finished

--- src/components/Pop.vue
+++ src/components/Pop.vue
@@ -87,6 +87,7 @@ function handleSubmit() {
     axios.post('/api/contact', requestOptions)
       .then((response: AxiosResponse) => {
         if (response.status === 200) {
+          closeModal()
           router.push('/thank-you')
         } else {
           throw new Error(`${response.status.toString()} error. Please try again later.`);
