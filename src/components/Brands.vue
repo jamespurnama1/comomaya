bd28caceef533d58c8f66ded31e869278f9d1540
jamespurnama1 <james.henry.purnama0@gmail.com>
2024-08-16 15:23:06 +0700

    spacing brands fixes more brands

--- src/components/Brands.vue
+++ src/components/Brands.vue
@@ -14,10 +14,10 @@ const boutiqueBrandsMobile = Object.values(import.meta.glob('@/assets/brands_mob
     <div
       class="max-w-[1920px] mx-auto flex flex-col justify-center relative min-h-[70vh] px-9 md:px-20 xl:px-36 py-10 md:py-20 lg:pt-16 gap-2 md:gap-10">
       <span>
-        <h2 class="text-blue drama text-5xl md:text-6xl lg:text-7xl font-semibold">
+        <h2 class="text-blue drama text-5xl md:text-6xl lg:text-7xl font-semibold -ml-2">
           our&nbsp;<br v-if="store.getWidth <= 768"><strong>clients</strong>
         </h2>
-        <p class="text-stone-500 text-sm md:text-lg font-semibold tracking-widest text-left pt-5">BOUTIQUE &amp;
+        <p class="text-blue text-sm md:text-lg font-semibold tracking-widest text-left">BOUTIQUE &amp;
           CORPORATE
           BRANDS</p>
       </span>
@@ -34,10 +34,10 @@ const boutiqueBrandsMobile = Object.values(import.meta.glob('@/assets/brands_mob
       </div>
 
       <span>
-        <h2 class="text-blue drama text-5xl md:text-6xl lg:text-7xl font-semibold mt-20">
+        <h2 class="text-blue drama text-5xl md:text-6xl lg:text-7xl font-semibold mt-20 -ml-2">
           brands <br v-if="store.getWidth <= 768"><strong>we have<br>worked&nbsp;with</strong>
         </h2>
-        <p class="text-stone-500 text-sm md:text-lg font-semibold tracking-widest text-left pt-5">GLOBAL BRANDS</p>
+        <p class="text-blue text-sm md:text-lg font-semibold tracking-widest text-left">GLOBAL BRANDS</p>
       </span>
       <div class="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4 md:gap-x-10 md:gap-y-16">
         <div v-if="store.getWidth <= 768" v-for="brand in globalBrandsMobile"
